79ccc8d349f219f4977ae5d171e34987601c5414
Erno Hentonen <erno.hentonen@opiframe.com>
2023-03-27 13:40:08 +0300

    javascript form

--- javascript/03_forms/public/script.js
+++ javascript/03_forms/public/script.js
@@ -1,5 +1,36 @@
-function changeColor() {
-	let header = document.getElementById("header");
-	header.style.color = "red";
-	console.log("Changed color");
+window.onload = function() {
+	let form = document.getElementById("form");
+	form.addEventListener("submit",submit)
+}
+
+function submit(event) {
+	event.preventDefault();
+	let username_input = document.getElementById("username");
+	let password_input = document.getElementById("password");
+	let username = username_input.value;
+	let password = password_input.value;
+	let user = {
+		"username":username,
+		"password":password
+	}
+	login(user);
+}
+
+async function login(user) {
+	let request = {
+		method:"POST",
+		headers:{
+			"Content-Type":"application/json"
+		},
+		body:JSON.stringify(user)
+	}
+	const response = await fetch("/login",request);
+	if(!response) {
+		return;
+	}
+	if(response.ok) {
+		console.log("Logged in!")
+	} else {
+		console.log("Unauthorized:"+response.status+" "+response.statusText)
+	}
 }
