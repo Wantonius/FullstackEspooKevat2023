b0653bc446e658e43020d60400927ef04aaaf3ab
Erno Hentonen <erno.hentonen@opiframe.com>
2023-03-28 11:18:10 +0300

    simple spa backend 2

--- javascript/05_simple_spa/server.js
+++ javascript/05_simple_spa/server.js
@@ -46,6 +46,30 @@ app.post("/api/contact", function(req,res) {
 	return res.status(201).json({"message":"Created"});
 })
 
+app.delete("/api/contact/:id",function(req,res) {
+	let tempId = parseInt(req.params.id,10);
+	database = database.filter(contact => contact.id !== tempId);
+	return res.status(200).json({"message":"Success"});
+})
+
+app.put("/api/contact/:id",function(req,res) {
+	let tempId = parseInt(req.params.id,10);
+	let contact = {
+		id:tempId,
+		firstname:req.body.firstname,
+		lastname:req.body.lastname,
+		email:req.body.email,
+		phone:req.body.phone
+	}
+	for(let i=0;i<database.length;i++) {
+		if(tempId === database[i].id) {
+			database.splice(i,1,contact);
+			return res.status(200).json({"message":"Success"})
+		}
+	}
+	return res.status(404).json({"message":"Not found"})
+})
+
 app.listen(3000);
 
 console.log("Running in port 3000");
