1e72ad254691cd21e3a00764141b374ec25a7533
Erno Hentonen <erno.hentonen@opiframe.com>
2023-03-28 09:57:51 +0300

    simple spa backend 1

---
+++ javascript/05_simple_spa/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+let app = express();
+
+app.use("/",express.static("public"));
+app.use(express.json());
+
+//DATABASE
+
+let database = [];
+let id = 100;
+
+//REST
+
+/*
+CREATE - POST "/api/contact"
+READ - GET "/api/contact"
+UPDATE - PUT "/api/contact/:id"
+DELETE - DELETE "/api/contact/:id"
+*/
+
+//contact Object
+/*
+	id:number,
+	firstname:string,
+	lastname:string,
+	email:string,
+	phone:string
+
+*/
+
+app.get("/api/contact",function(req,res) {
+	return res.status(200).json(database);
+})
+
+app.listen(3000);
+
+console.log("Running in port 3000");
+
