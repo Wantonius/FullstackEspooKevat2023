98c689167fd7d19608986ec819d055dffb39a921
Erno Hentonen <erno.hentonen@opiframe.com>
2023-03-28 10:11:57 +0300

    simple spa backend 1

--- javascript/05_simple_spa/server.js
+++ javascript/05_simple_spa/server.js
@@ -33,6 +33,19 @@ app.get("/api/contact",function(req,res) {
 	return res.status(200).json(database);
 })
 
+app.post("/api/contact", function(req,res) {
+	let contact = {
+		id:id,
+		firstname:req.body.firstname,
+		lastname:req.body.lastname,
+		email:req.body.email,
+		phone:req.body.phone
+	}
+	id++;
+	database.push(contact);
+	return res.status(201).json({"message":"Created"});
+})
+
 app.listen(3000);
 
 console.log("Running in port 3000");
