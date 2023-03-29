db1dc4dfcb9d65c7a910ee2c0a7a09aab0b77647
Erno Hentonen <erno.hentonen@opiframe.com>
2023-03-29 14:41:37 +0300

    currying

--- javascript/09_currying/public/script.js
+++ javascript/09_currying/public/script.js
@@ -2,6 +2,21 @@ function start() {
 	console.log("----- Simple currying function -----");
 	console.log(sum(4)(2));
 	console.log(sum(2));
+	
+	console.log("----- Timer async/sync currying -----");
+	console.log("Sync function",timer(syncFunc)(200));
+	console.log("Async function");
+	console.log(timer(asyncFunc)(400).then(response => console.log(response)));
+}
+
+const timer = (timerFunc) => (...args) => {
+	const start = Date.now();
+	const value = timerFunc(...args);
+	if(value && typeof value.then === "function") {
+		return value.then(value => ({timespan:Date.now() - start,value}))
+	} else {
+		return {timespan:Date.now()-start,value}
+	}
 }
 
 const sum = x => y => x+y;
