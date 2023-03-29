63adcdf66b005f3b9a9259147fc0d50e6ea8f366
Erno Hentonen <erno.hentonen@opiframe.com>
2023-03-29 11:19:17 +0300

    closures

--- javascript/06_closures/public/script.js
+++ javascript/06_closures/public/script.js
@@ -1,3 +1,29 @@
+window.onload = function() {
+	let fontSizer = changeFontSize();
+	const biggerButton = document.getElementById("bigger");
+	bigger.onclick = fontSizer.bigger;
+	const smallerButton = document.getElementById("smaller");
+	smaller.onclick = fontSizer.smaller;
+}
+
+const changeFontSize = function() {
+	let fontSize = 16;
+	document.body.style.fontSize = fontSize+"px";
+	
+	function changeSize(val) {
+		fontSize += val;
+		document.body.style.fontSize = fontSize+"px";
+	}
+	return {
+		bigger:function() {
+			changeSize(2);
+		},
+		smaller:function() {
+			changeSize(-2);
+		}
+	}
+}
+
 const makeCounter = function() {
 	let privateCounter = 0;
 	
