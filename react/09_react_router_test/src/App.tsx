50e816fe949cf647e847776327b798f6766f7154
Erno Hentonen <erno.hentonen@opiframe.com>
2023-04-13 09:51:15 +0300

    react router

--- react/09_react_router_test/src/App.tsx
+++ react/09_react_router_test/src/App.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Secret from './components/Secret';
+import {Routes,Route,Link,Navigate} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<ul style={{"listStyleType":"none"}}>
+			<li><Link to="/">Home</Link></li>
+			<li><Link to="/about">About</Link></li>
+		</ul>
+		<Routes>
+			<Route path="/" element={<Home/>}/>
+			<Route path="/about" element={<About/>}/>
+			<Route path="/secret" element={<Secret/>}/>
+			<Route path="*" element={<Navigate to="/"/>}/>
+		</Routes>
     </div>
   );
 }
