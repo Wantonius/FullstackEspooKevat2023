bd920ccd4bcbee4304d5df46f2149eceaad66e12
Erno Hentonen <erno.hentonen@opiframe.com>
2023-04-24 09:53:16 +0300

    redux 1

--- react/11_redux_test_1/src/App.tsx
+++ react/11_redux_test_1/src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import {useDispatch,useSelector} from 'react-redux';
+import {State} from './reducers/countReducer';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	
+	const dispatch = useDispatch();
+	
+	const stateSelector = (state:State) => state;
+	const state:State = useSelector(stateSelector);
+	
+	return (
+		<div className="App">
+			<h3>Current value:{state.count}</h3>
+			<button onClick={() => dispatch({type:"INCREMENT"})}>+</button>
+			<button onClick={() => dispatch({type:"DECREMENT"})}>-</button>
+		</div>
+	);
 }
 
 export default App;
